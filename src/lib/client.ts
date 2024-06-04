cff85900646e2adc6a8da870ff788c5fc58e0bb8
Rafael Campos de Bastiani <rafael@Rafaels-MacBook-Pro.local>
2024-06-04 16:39:59 -0300

    fix(env): fix env

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,5 +1,5 @@
 import { Client } from "@hyper-fetch/core";
 
 export const client = new Client({
-  url: process.env.PUBLIC_VERCEL_URL || "http://localhost:3000",
+  url: process.env.NEXT_PUBLIC_VERCEL_URL || "",
 }).setDebug(true);
